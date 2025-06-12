8616deee83c591c161ab5e19c9bea494b6d277ed
Wisdom Asigbey <113071078+WisdomAsigbey@users.noreply.github.com>
2025-06-12 12:22:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+document.addEventListener("click", function (event) {
+  let dropdown = document.getElementById("dropdown_1");
+  let button = document.getElementById("btn_1");
+
+  // Check if the clicked element is NOT the dropdown or the button
+  if (!dropdown.contains(event.target) && !button.contains(event.target)) {
+    dropdown.classList.add("d-none");
+  }
+});
+document.addEventListener("click", function (event) {
+  let dropdown = document.getElementById("dropdown_2");
+  let button = document.getElementById("btn_2");
+
+  // Check if the clicked element is NOT the dropdown or the button
+  if (!dropdown.contains(event.target) && !button.contains(event.target)) {
+    dropdown.classList.add("d-none");
+  }
+});
+
 const xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
 const yValues = [55, 49, 44, 24, 15];
 const barColors = ["red", "green", "blue", "orange", "brown"];
-
+// chart
 new Chart("myChart", {
   type: "line",
   data: {
@@ -14,3 +33,22 @@ new Chart("myChart", {
     ],
   },
 });
+
+// dropdown
+function toggleDropdown_1() {
+  let dropdown = document.getElementById("dropdown_1");
+  if (dropdown.classList.contains("d-none")) {
+    dropdown.classList.remove("d-none");
+  } else {
+    dropdown.classList.add("d-none");
+  }
+}
+
+function toggleDropdown_2() {
+  let dropdown = document.getElementById("dropdown_2");
+  if (dropdown.classList.contains("d-none")) {
+    dropdown.classList.remove("d-none");
+  } else {
+    dropdown.classList.add("d-none");
+  }
+}
